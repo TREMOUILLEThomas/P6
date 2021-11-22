287ce046365c93b0e53d4142b250bb982b6194c7
TREMOUILLEThomas <90332661+TREMOUILLEThomas@users.noreply.github.com>
2021-11-22 08:20:12 +0100

    Add files via upload

---
+++ ex90.py
@@ -0,0 +1,6 @@
+def nb(r):
+    e=0
+    while r>1:
+        r=r/10
+        e=e+1
+    return (e)
---
+++ ex91.py
@@ -0,0 +1,7 @@
+a=in(input("quel nombre ? : "))
+while a>1:
+    if a%2==1
+        a=a*3+1
+    else:
+        a=a//2
+    print(a)
